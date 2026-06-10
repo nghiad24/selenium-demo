a5970350a6f3674f03c2f21d6857c8fb01d2966a
nghiad24 <nghiadinh240103@gmail.com>
2026-06-10 11:51:40 +0700

    Initial Selenium Test

---
+++ src/main/java/com/example/LoginPage.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class LoginPage 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/example/LoginTest.java
@@ -0,0 +1,33 @@
+package com.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    @Test
+    public void loginSuccess() {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.saucedemo.com");
+
+        driver.findElement(By.id("user-name"))
+                .sendKeys("standard_user");
+
+        driver.findElement(By.id("password"))
+                .sendKeys("secret_sauce");
+
+        driver.findElement(By.id("login-button"))
+                .click();
+
+        Assert.assertTrue(
+                driver.getCurrentUrl().contains("inventory")
+        );
+
+        driver.quit();
+    }
+}
