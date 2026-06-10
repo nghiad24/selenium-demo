297a2be29415326e37c70eb7bbfd93037d2532fa
nghiad24 <nghiadinh240103@gmail.com>
2026-06-10 14:48:21 +0700

    feat: split tests into success and failure test files

---
+++ src/test/java/com/nghia/LoginFailureTest.java
@@ -0,0 +1,40 @@
+package com.nghia;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginFailureTest {
+
+    @Test
+    public void loginFailure() {
+
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://www.saucedemo.com");
+
+        driver.findElement(By.id("user-name"))
+                .sendKeys("standard_user");
+
+        driver.findElement(By.id("password"))
+                .sendKeys("123456");
+
+        driver.findElement(By.id("login-button"))
+                .click();
+
+        Assert.assertFalse(
+                driver.getCurrentUrl().contains("inventory")
+        );
+
+        driver.quit();
+    }
+}
--- src/test/java/com/nghia/LoginSuccessTest.java
+++ src/test/java/com/nghia/LoginSuccessTest.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class LoginTest {
+public class LoginSuccessTest {
 
     @Test
     public void loginSuccess() {
@@ -32,35 +32,6 @@ public class LoginTest {
                 .click();
 
         Assert.assertTrue(
-                driver.getCurrentUrl().contains("inventory")
-        );
-
-        driver.quit();
-    }
-
-    @Test
-    public void loginFailure() {
-
-        ChromeOptions options = new ChromeOptions();
-
-        options.addArguments("--headless=new");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-
-        WebDriver driver = new ChromeDriver(options);
-
-        driver.get("https://www.saucedemo.com");
-
-        driver.findElement(By.id("user-name"))
-                .sendKeys("standard_user");
-
-        driver.findElement(By.id("password"))
-                .sendKeys("123456");
-
-        driver.findElement(By.id("login-button"))
-                .click();
-
-        Assert.assertFalse(
                 driver.getCurrentUrl().contains("inventory")
         );
 
