c8afe73c42266311fd14b670730a2b30f3bdef71
nghiad24 <nghiadinh240103@gmail.com>
2026-06-10 14:33:17 +0700

    Fix GitHub Actions Chrome headless

--- src/test/java/com/DinhVanNghia/LoginTest.java
+++ src/test/java/com/DinhVanNghia/LoginTest.java
@@ -26,7 +26,7 @@ public class LoginTest {
                 .sendKeys("standard_user");
 
         driver.findElement(By.id("password"))
-                .sendKeys("1234567");
+                .sendKeys("secret_sauce");
 
         driver.findElement(By.id("login-button"))
                 .click();
